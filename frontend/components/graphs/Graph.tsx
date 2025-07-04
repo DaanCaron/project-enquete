7364eb38a358138590c6bad93d23cf7f1fe951df
DaanCaron <daan.caron@student.ucll.be>
2025-07-04 21:08:29 +0200

    graph files added

---
+++ frontend/components/graphs/Graph.tsx
@@ -0,0 +1,9 @@
+const Graph:React.FC = () => {
+    return(
+        <div>
+            :D
+        </div>
+    )
+}
+
+export default Graph
---
+++ frontend/pages/graph/index.tsx
@@ -0,0 +1,11 @@
+import Graph from "@/components/graphs/Graph"
+
+const Index:React.FC = () =>{
+    return(
+        <div className="bg-[#424242] h-screen w-screen">
+            <Graph />
+        </div>
+    )
+}
+
+export default Index
